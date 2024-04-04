d0b8ab7a64e23f5ef36dddf20042ee9089d506a7
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2024-04-04 11:47:57 +0200

    :sparkles: Ajout des routes #6 #8

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {ConnexionComponent} from "./components/connexion/connexion.component";
+import {RegisterComponent} from "./components/register/register.component";
 
 export const routes: Routes = [
-  {path: "", pathMatch: "full", redirectTo: ''},
+  {path: "", pathMatch: "full", redirectTo: '/'},
+  {path: "login", component: ConnexionComponent},
+  {path: "register", component: RegisterComponent},
+  {path: "**", component: PageNotFoundComponent}
 ];
