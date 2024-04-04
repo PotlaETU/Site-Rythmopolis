11ab01a9a90f48dabd54a0f9fc9d7b2047eaaad8
Anthony Rolland <anthony_rolland@ens.univ-artois.fr>
2024-04-04 11:16:25 +0200

    :sparkles: Gestion de variables #6 #8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'AngularSite';
+  image = 'assets/avatar.png'
+  connection: string = 'Se connecter';
+  logo: string = 'assets/logo.png';
 }
