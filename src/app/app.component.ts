70702b59c9873d29ac77a9226997aedffdcf206e
Just Vallin--Detrez <just_vallin--detrez@ens.univ-artois.fr>
2024-04-04 11:35:40 +0200

    Le composant accueil #8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {AccueilComponent} from "./components/accueil/accueil.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgOptimizedImage],
+  imports: [CommonModule, RouterOutlet, NgOptimizedImage, AccueilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
