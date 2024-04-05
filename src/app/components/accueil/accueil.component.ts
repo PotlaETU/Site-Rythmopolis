2b097ea66ec359e5e6e45e2ed33c69dc0936b8d4
Allaert Damien <damien_allaert@ens.univ-artois.fr>
2024-04-05 10:31:06 +0200

    Ajout des certaines information dans la liste #10

--- src/app/components/accueil/accueil.component.ts
+++ src/app/components/accueil/accueil.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {MessageService} from "../../services/message.service";
 
 @Component({
   selector: 'app-accueil',
@@ -9,5 +10,15 @@ import { RouterLink } from '@angular/router';
   styleUrl: './accueil.component.css'
 })
 export class AccueilComponent {
+
+  message: string | null = null;
+
+  constructor(private messageService: MessageService) {
+  }
+
+  ngOnInit() {
+    this.message = this.messageService.getMessage();
+    this.messageService.setMessage('');
+  }
 
 }
