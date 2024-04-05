814f9921e7143345a38c7df1f09a6df97eb79021
Just Vallin--Detrez <just_vallin--detrez@ens.univ-artois.fr>
2024-04-05 10:56:26 +0200

    La page contact #26

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { ConnexionComponent } from "./components/connexion/connexion.component";
 import { RegisterComponent } from "./components/register/register.component";
 import { AccueilComponent } from './components/accueil/accueil.component';
 import {AProposComponent} from "./components/a-propos/a-propos.component";
+import {ContactComponent} from "./components/contact/contact.component";
 
 export const routes: Routes = [
   { path: "", pathMatch: "full", component: AccueilComponent },
   { path: "login", component: ConnexionComponent },
   { path: "register", component: RegisterComponent },
   { path : "about", component: AProposComponent },
+  { path : "contact", component: ContactComponent },
   { path: "**", component: PageNotFoundComponent }
 ];
