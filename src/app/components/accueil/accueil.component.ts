82b175fab1dc71536a582e98838adfe2a51a4c85
Just Vallin--Detrez <just_vallin--detrez@ens.univ-artois.fr>
2024-04-05 09:29:42 +0200

    Le composant accueil #8

--- src/app/components/accueil/accueil.component.ts
+++ src/app/components/accueil/accueil.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterLink } from '@angular/router';
+import {EventService} from "../../services/event.service";
+import {Evenement} from "../../models/evenement";
 
 @Component({
   selector: 'app-accueil',
@@ -9,4 +11,14 @@ import { RouterLink } from '@angular/router';
   styleUrl: './accueil.component.css'
 })
 export class AccueilComponent {
+
+  event:EventService = inject(EventService);
+  selectedEvent: Evenement | null = null;
+
+  eventAleatoire(){
+    this.event.events().subscribe((e)=>{
+      let index = Math.floor(Math.random() * e.length);
+      this.selectedEvent = e[index];
+    })
+  }
 }
