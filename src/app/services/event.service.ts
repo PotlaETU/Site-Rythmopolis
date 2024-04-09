725665427284e8ae70ddbb0a896f1b3430a4417f
Just Vallin--Detrez <just_vallin--detrez@ens.univ-artois.fr>
2024-04-09 17:03:27 +0200

    La liste des événements #13

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -5,6 +5,7 @@ import {catchError, map} from 'rxjs/operators';
 import {environment} from "../../environments/environments";
 import {Evenement} from "../models/evenement";
 import {Client} from "../models/client";
+import {Artiste} from "../models/artiste";
 
 @Injectable({
   providedIn: 'root'
@@ -28,5 +29,24 @@ export class EventService {
     );
   }
 
+  getParticipants(id: number): Observable<any> {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    const token = user.token;
+    const url = `${environment.apiURL}/evenements/${id}`;
+
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json', 'authorization': `bearer ${token}`})
+    };
+
+    console.log('getParticipants')
+    return this.http.get<{evenement: Evenement}>(url, httpOptions).pipe(
+      map(res =>
+        res.evenement.artistes
+      ),catchError(err => {
+        console.log('Erreur http : ', err);
+        return of([]);
+      }),
+    );
+  }
 }
 
