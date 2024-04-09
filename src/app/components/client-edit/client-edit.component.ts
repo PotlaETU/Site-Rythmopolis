4bbe703d5fcd577f2d99378bdebe27ca9b39b1b4
Allaert Damien <damien_allaert@ens.univ-artois.fr>
2024-04-09 15:19:21 +0200

    Modification du edit pour modifier le user d'un client

--- src/app/components/client-edit/client-edit.component.ts
+++ src/app/components/client-edit/client-edit.component.ts
@@ -1,8 +1,13 @@
-import {Component, Injectable} from '@angular/core';
+import {Component, inject, Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {environment} from "../../../environments/environments";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Role} from "../../models/role";
+import {User} from "../../models/user";
+import {ClientService} from "../../services/client.service";
+import {Client} from "../../models/client";
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +15,8 @@ import {environment} from "../../../environments/environments";
 export class UserService {
   constructor(private http: HttpClient) { }
 
-  updateUser(data: any): Observable<any> {
-    const url = `${environment.apiURL}/clients`;
+  updateUser(id: number | undefined, data: any): Observable<any> {
+    const url = `${environment.apiURL}/clients/${id}`;
     const httpOptions = {
       headers: new HttpHeaders({'Content-Type': 'application/json'})
     };
@@ -29,29 +34,61 @@ export class UserService {
   styleUrl: './client-edit.component.css'
 })
 export class ClientEditComponent {
-  editForm: FormGroup = this.fb.group({
-    name: ['', Validators.required],
-    adresse: ['', Validators.required],
-    password: ['', Validators.required],
-    confirmPassword: ['', Validators.required],
-    role: ['', Validators.required]
-  }, {validator: this.checkPasswords });
+  router = inject(Router);
+  clientService = inject(ClientService);
+  user? : User;
+  id: number = 0
+  client?: Client;
 
-  roles: string[] = ['Gestionnaire', 'Admin', 'Actif'];
+  editForm: FormGroup = new FormGroup({
+    name: new FormControl([this.user?.nom], Validators.required),
+    email: new FormControl([this.user?.email], Validators.required),
+    password: new FormControl([], Validators.required),
+    confirmPassword: new FormControl([''], Validators.required),
+    role: new FormControl([this.user?.role], Validators.required),
+  });
 
-  constructor(private fb: FormBuilder, private userService: UserService) { }
+
+  get name() {
+    return this.editForm.get('name');
+  }
+
+  get email() {
+    return this.editForm.get('adresse');
+  }
+
+  get password() {
+    return this.editForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.editForm.get('confirmPassword');
+  }
+
+  get role() {
+    return this.editForm.get('role');
+  }
+
+  constructor(private fb: FormBuilder, private userService: UserService, private route : ActivatedRoute) { }
+
+  ngOnInit() {
+    this.id = +(this.route.snapshot.paramMap.get('id') || 0);
+    this.clientService.getUser(this.id.toString()).subscribe(user => { this.user = user; this.editForm.patchValue(user)});
+  }
 
 
   checkPasswords(group: FormGroup) {
     let pass = group.get('password')?.value;
     let confirmPass = group.get('confirmPassword')?.value;
 
-    return pass === confirmPass ? null : { notSame: true }
+    return pass === confirmPass
   }
 
   onSubmit() {
-    if (this.editForm.valid) {
-      this.userService.updateUser(this.editForm.value).subscribe();
+    if (this.editForm.valid && this.checkPasswords(this.editForm)){
+      this.userService.updateUser(this.user?.id, this.editForm.value).subscribe();
+      console.log('User updated successfully');
+      this.router.navigate([`/clients/${this.user?.id}`]);
     }
   }
 }
