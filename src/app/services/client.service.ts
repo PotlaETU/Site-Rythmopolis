775e40048fad72e2442706ae4eb8fa0858226140
Allaert Damien <damien_allaert@ens.univ-artois.fr>
2024-04-09 10:29:39 +0200

    :construction: Début client service

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -4,6 +4,9 @@ import {Client} from "../models/client";
 import {map, Observable, of} from "rxjs";
 import {catchError} from "rxjs/operators";
 import {environment} from "../../environments/environments";
+import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
+import {User} from "../models/user";
+import {FormGroup} from "@angular/forms";
 
 @Injectable({
   providedIn: 'root'
@@ -12,28 +15,56 @@ export class ClientService {
 
   constructor(protected http : HttpClient) { }
 
-  getClients() {
+  getClients():Observable<Client[]> {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    const token = user.token;
     const url = `${environment.apiURL}/clients`;
+
     const httpOptions = {
-      headers: new HttpHeaders({'Content-Type': 'application/json'})
+      headers: new HttpHeaders({'Content-Type': 'application/json', 'authorization': `bearer ${token}`})
     };
 
-    return this.http.get<{data: Client[]}>(url, httpOptions).pipe(
-      map(res => res.data),
-      catchError(err => {
+    // const headers = new HttpHeaders().set('Authorization', `bearer ${token}`);
+    // headers.set('Content-Type', 'application/json');
+    // console.log(headers.get('Authorization'))
+
+    this.http.get<{clients: Client[]}>(url, httpOptions).pipe(
+      map(res =>
+        res.clients
+      )).subscribe(clients => console.log(clients));
+
+    return this.http.get<{clients: Client[]}>(url, httpOptions).pipe(
+      map(res =>
+        res.clients
+      ),catchError(err => {
         console.log('Erreur http : ', err);
         return of([]);
       }),
     );
   }
 
-  getClient(id: number): Observable<Client> {
+  getClient(id: string): Observable<Client> {
     const url = `${environment.apiURL}/clients/${id}`;
+    const token = JSON.parse(localStorage.getItem('user') || '{}').token;
     const httpOptions = {
-      headers: new HttpHeaders({'Content-Type': 'application/json'})
+      headers: new HttpHeaders({'Content-Type': 'application/json', 'authorization': `bearer ${token}`})
     };
-    return this.http.get<{data: Client}>(url, httpOptions).pipe(
-      map(res => res.data)
+    return this.http.get<{clients: Client}>(url, httpOptions).pipe(
+      map(res => res.clients)
+    );
+  }
+
+  setClient(id:string, form: FormGroup):Observable<Client>{
+    const url = `${environment.apiURL}/clients/${id}`;
+    const token = JSON.parse(localStorage.getItem('user') || '{}').token;
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json', 'authorization': `bearer ${token}`})
+    };
+    return this.http.put<{clients: Client}>(url, form.value ,httpOptions).pipe(
+      map(res => res.clients),catchError(err => {
+        console.log('Erreur SetClients : ', err);
+        return of();
+      })
     );
   }
 }
